902fc7e3abd33c2e2233bd6f4c62481865c22bea
Mifou <dzigsaw@wp.pl>
2020-05-17 16:48:08 +0200

    collectionTasks1 added

---
+++ main/com/collections/Methods.java
@@ -0,0 +1,30 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Methods {
+
+    public ArrayList<Integer> putToArrayAfterElement(ArrayList<Integer> array, int elementToPut, int existingElement, boolean isLast) {
+        if (array.contains(existingElement)) {
+            if (!isLast) {
+                array.add((array.indexOf(existingElement)) + 1, elementToPut);
+            } else {
+                array.add((array.lastIndexOf(existingElement)) + 1, elementToPut);
+            }
+            return array;
+
+        }
+        return array;
+    }
+
+    static boolean compare(Set<?> set1, Set<?> set2) {
+        if (set1 == null || set2 == null) {
+            return false;
+        }
+        if (set1.size() != set2.size()) {
+            return false;
+        }
+        return (set1.containsAll(set2));
+    }
+}
---
+++ test/com/collections/SalaryComparatorTest.java
@@ -0,0 +1,41 @@
+package com.collections;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryComparatorTest {
+
+    @Test
+    void salaryComparatorTest() {
+        Employee employee1 = new Employee("Kazek", 2000);
+        Employee employee2 = new Employee("Dzolek", 2500);
+        Employee employee3 = new Employee("Bolek", 3000);
+        Employee employee4 = new Employee("Jasiu", 3500);
+        Employee employee5 = new Employee("Pietrek Długos", 4000);
+
+        SalaryComparator salaryComparator = new SalaryComparator();
+
+        SortedSet<Employee> employeeSet1 = new TreeSet<Employee>(salaryComparator) {
+        };
+
+        employeeSet1.add(employee1);
+        employeeSet1.add(employee2);
+        employeeSet1.add(employee3);
+        employeeSet1.add(employee4);
+        employeeSet1.add(employee5);
+
+        ArrayList<Employee> employeeList = new ArrayList<>(employeeSet1);
+
+        ListIterator<Employee> iterator = employeeList.listIterator();
+
+        while (iterator.hasNext()) {
+            int firstSalary = iterator.next().getSalary();
+            if (iterator.hasNext()) {
+               int secondSalary = iterator.next().getSalary();
+                System.out.println(firstSalary + "   " + secondSalary);
+                assertTrue(firstSalary < secondSalary);
+                iterator.previous();
+            }
+        }
+    }
+}
